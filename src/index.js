d3e377be4d46c8253ad353f94228e60a2d6f7f25
Santiago <sntg98@hotmail.com>
2022-02-05 11:09:16 -0500

    Conexion a la base de datos

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const routes = require("./routes/routes")
+
 
 const app = express()
 const PORT = 4000
 
+// end-points
+app.use(routes)
 
 app.listen(PORT)
 console.log('server on port', PORT)
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,24 +1,14 @@
 // Configuración  end point
 const { Router } = require("express");
+const pool = require('../db')
 const router = Router()
 
-
 // comprobar conexión a la base de datos
-router.get('/task', async (req,res) => {
+router.get('/', async (req,res) => {
     const result = await pool.query('SELECT NOW()')
     console.log(result)
     res.json('get task') 
 })
 
-
-// router.get('/task', getAllTasks)
-
-// router.get('/task/:id', getTask)
-
-// router.post('/task', createTask)
-
-// router.delete('/task/:id', deleteTask)
-
-// router.put('/task/:id', updateTask)
 
 module.exports = router
